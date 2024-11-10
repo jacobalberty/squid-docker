6840118d3769685e9f46519cd2ed88e30522ebf8
Jacob Alberty <jacob.alberty@gmail.com>
2024-11-10 04:22:10 -0600

    Clean up squid pid file

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
+if [ -f "/var/run/squid.pid" ]; then
+  # Clean up squid pid file
+  rm -f /var/run/squid.pid
+fi
+
 if [[ "$1" == "squid" ]]; then
-    shift;
-    /usr/sbin/squid ${@}
+  shift;
+  /usr/sbin/squid ${@}
 else
-    exec ${@}
+  exec ${@}
 fi
 
